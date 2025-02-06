fd6ed4b7c08bb66734c5cbe9793a9325c7e8b347
RijinRaju <rijinraju856@gmail.com>
2025-02-06 10:20:27 +0530

    Fix: rebranded the names

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -109,7 +109,7 @@ calendars = ["Task", "Work Order", "Sales Order", "Holiday List", "ToDo"]
 website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
-	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
+	"favicon": "/assets/erpnext/images/thinkNXG-logo.jpg",
 	"splash_image": "/assets/erpnext/images/thinkNXG-logo.jpg",
 }
 
